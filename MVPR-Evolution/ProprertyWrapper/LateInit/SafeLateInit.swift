2015e856ccccf6a856c9efef952c6f308ca259ab
Josh Woomin Park <joshuaparkwm@gmail.com>
2020-06-04 08:40:59 +0900

    Add: LateInit & SafeLateInit

---
+++ MVPR-Evolution/ProprertyWrapper/LateInit/SafeLateInit.swift
@@ -0,0 +1,36 @@
+//
+//  SafeLateInit.swift
+//  MVPR-Evolution
+//
+//  Created by Josh Woomin Park on 2020/06/04.
+//  Copyright © 2020 Knowre. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * - Warning: 해당 property wrapper로 감싼 property의 경우 `willSet/didSet` 옵저버를 사용할 수 없음.
+ * (`willSet/didSet`에 unwrapped property가 넘겨지기 때문).
+ *
+ * 대신 `LateInit<T>.willSet/didSet`을 사용해야 함.
+ * 또한 `willSet/didSet`에 넘겨지는 값을 사용하고 직접 property에 접근해서는 안 됨.
+ */
+@propertyWrapper
+struct SafeLateInit<T> {
+    
+    var wrappedValue: T {
+        get { projectedValue }
+        set { projectedValue = newValue }
+    }
+    
+    @SafeReference(value: nil)
+    var projectedValue: T! {
+        willSet { willSet?(projectedValue, newValue) }
+        didSet { didSet?(oldValue, projectedValue) }
+    }
+    
+    var willSet: ((T?, T) -> Void)?
+    var didSet: ((T?, T) -> Void)?
+    
+    init() { }
+}
