682b8575344bc27efe030c0eefbeacf6707806f7
Josh Woomin Park <joshuaparkwm@gmail.com>
2020-06-04 10:33:41 +0900

    Update: [WIP] Move base files to "Base" target

---
+++ Base/Base.h
@@ -0,0 +1,19 @@
+//
+//  Base.h
+//  Base
+//
+//  Created by Josh Woomin Park on 2020/06/04.
+//  Copyright © 2020 Knowre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Base.
+FOUNDATION_EXPORT double BaseVersionNumber;
+
+//! Project version string for Base.
+FOUNDATION_EXPORT const unsigned char BaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base/PublicHeader.h>
+
+
